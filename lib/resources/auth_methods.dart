68fd9238b68cb1b2ad1c13b5f00b88a05a4ce51c
BATUHAN UZUNOGLU <batuhan.uzunoglu@ext.digiturk.com.tr>
2022-12-09 00:43:59 +0300

    Create auth methods class

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,14 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //sign up user
-  signUpUser({
+  Future<String> signUpUser({
     required String email,
     required String password,
     required String username,
     required String bio,
-  }) {}
+  }) async {
+    String res = "Some error occured";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        //Create a user firebase authentication
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        print(cred.user!.uid);
+        //add user to database
+        _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'subjects': [],
+        });
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
 }
